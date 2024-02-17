6eed9a1c8226594a76691fecc3da4e1c09f3e762
gashon <gashon96@gmail.com>
2024-02-16 21:47:38 -0800

    feat: auth routes

---
+++ src/pages/api/auth/magic.ts
@@ -0,0 +1,34 @@
+import { AUTH_COOKIE } from "@/consts";
+import { AuthToken, LoginPostRequest } from "@/types";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { createToken } from "@/lib/jwt";
+import { sendLoginEmail } from "@/util/send-email";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== "POST") {
+    res.json({
+      message: "Method not allowed",
+    });
+
+    return;
+  }
+
+  const { email } = req.body as LoginPostRequest;
+  const token = createToken<AuthToken>({
+    email,
+    created_at: new Date().getTime(),
+  });
+
+  const loginLink = `${process.env.NEXT_PUBLIC_BASE_URL}/auth/register?token=${token}`;
+
+  await sendLoginEmail({ email, loginLink });
+
+  res.json({
+    message: "success",
+  });
+
+  return;
+}
---
+++ src/pages/api/auth/register.ts
@@ -0,0 +1,56 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { AUTH_COOKIE } from "@/consts";
+import { createToken, verifyToken } from "@/lib/jwt";
+import { AuthToken, RegisterPostRequest } from "@/types";
+import { db } from "@/db";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== "POST") {
+    res.json({
+      message: "Method not allowed",
+    });
+
+    return;
+  }
+
+  const { token: registrationToken } = req.body as RegisterPostRequest;
+
+  let token: AuthToken;
+  try {
+    token = verifyToken<AuthToken>(registrationToken);
+  } catch (err) {
+    res.status(401).json({ message: "Failed to parse" });
+    return;
+  }
+
+  // TODO(@gashon) update query to insert on duplicate key
+  let user = await db
+    .selectFrom("user")
+    .select("email")
+    .where("email", "=", token.email)
+    .executeTakeFirst();
+
+  if (!user)
+    await db
+      .insertInto("user")
+      .values({
+        email: token.email,
+      })
+      .execute();
+
+  // attach to cookie
+  res.setHeader(
+    "Set-Cookie",
+    `${AUTH_COOKIE}=${registrationToken}; HttpOnly; Path=/; Max-Age=0`,
+  );
+
+  res.json({
+    message: "success",
+  });
+
+  return;
+}
