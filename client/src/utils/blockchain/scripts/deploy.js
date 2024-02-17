0a911c57ac07b03cfe20cd8f6ec67d37c18a6c3f
Ameya Jadhav <jadhavameyak@gmail.com>
2024-02-16 23:03:13 -0800

    chore: audio contract update

--- client/src/utils/blockchain/scripts/deploy.js
+++ client/src/utils/blockchain/scripts/deploy.js
@@ -1,11 +1,11 @@
 // scripts/deploy.js
 async function main() {
   // 1. Get the contract to deploy
-  const Your_Contract = await ethers.getContractFactory('HelloWorld');
+  const Your_Contract = await ethers.getContractFactory('AudioContract');
   console.log('Deploying Your_Contract...');
 
   // 2. Instantiating a new smart contract
-  const your_contract = await Your_Contract.deploy('message');
+  const your_contract = await Your_Contract.deploy('audio_file_name');
 
   // 3. Waiting for the deployment to resolve
   await your_contract.deployed();
