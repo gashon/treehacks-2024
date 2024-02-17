71434c83f36e350492541db41dfee12e4fe15e46
Ameya Jadhav <jadhavameyak@gmail.com>
2024-02-17 00:16:26 -0800

    feat: chain updates

--- client/src/utils/blockchain/scripts/eth.js
+++ client/src/utils/blockchain/scripts/eth.js
@@ -1,44 +1,83 @@
 const { ethers } = require('ethers');
-const { privateKey, calderaRPCUrl, calderaChainId } = require('./secrets.json');
+const {
+  privateKey,
+  calderaRPCUrl,
+  calderaChainId,
+} = require('../secrets.json');
+var solc = require('solc');
+
+var contract = `
+  pragma solidity >=0.7.3;
+
+  contract AudioContract {
+    string public userId;
+    string public songId;
+    string public createdAt;
+
+    constructor(string memory initUserId, string memory initSongId, string memory initCreatedAt) {
+
+        userId = initUserId;
+        songId = initSongId;
+        createdAt = initCreatedAt;
+    }
+
+    function updateUserId(string memory newUserId) public {
+        string memory oldUserId = userId;
+        userId = newUserId;
+    }
+
+    function updateSongId(string memory newSongId) public {
+        string memory oldSongId = songId;
+        songId = newSongId;
+    }
+
+    function updateCreatedAt(string memory newCreatedAt) public {
+        string memory oldCreatedAt = createdAt;
+        createdAt = newCreatedAt;
+    }
+  }`;
+
+var input = {
+  language: 'Solidity',
+  sources: { 'audio.sol': { content: contract } },
+  settings: { outputSelection: { '*': { '*': ['*'] } } },
+};
+
+var output = JSON.parse(solc.compile(JSON.stringify(input)));
+var contractName = 'AudioContract';
+var bytecode = output.contracts['audio.sol'][contractName].evm.bytecode.object;
+var abi = output.contracts['audio.sol'][contractName].abi;
 
 // Connect to the blockchain with your RPC provider
-const providerRPC = {
-  caldera: {
-    name: 'Caldera',
-    rpc: calderaRPCUrl, // Insert your RPC URL here
-    chainId: calderaChainId, //Insert your ChainID Here
-  },
-};
-const provider = new ethers.providers.StaticJsonRpcProvider(
-  providerRPC.constellation.rpc,
-  {
-    chainId: providerRPC.constellation.chainId,
-    name: providerRPC.constellation.name,
-  }
-);
+// const providerRPC = {
+//   caldera: {
+//     name: 'Caldera',
+//     rpc: calderaRPCUrl,
+//     chainId: calderaChainId,
+//   },
+// };
+const provider = new ethers.providers.JsonRpcProvider(calderaRPCUrl);
 
-// Wallet/Private key of the deployer
 const wallet = new ethers.Wallet(privateKey, provider);
 
-// Compiled contract ABI and Bytecode
-const contractABI = 'your-contract-abi';
-const contractBytecode = 'your-contract-bytecode';
+var contractABI = abi;
+var contractBytecode = bytecode;
 
-async function deployContract() {
+async function deployContract(userid, songid, createdAt) {
   // Create a Contract Factory
   const factory = new ethers.ContractFactory(
     contractABI,
     contractBytecode,
     wallet
   );
-  const contract = await factory.deploy(); // Deploy the contract
+  const contract = await factory.deploy(userid, songid, createdAt); // Deploy the contract
 
-  console.log(`Contract deployed to address: ${contract.address}`);
+  await contract.deployed();
+
+  console.log('Your_Contract deployed to:', contract.address);
 }
 
-deployContract();
-
-//
+deployContract('userid', 'songid', 'createdAt');
 
 async function setAndGet(contractAddress) {
   const contract = new ethers.Contract(contractAddress, contractABI, wallet);
@@ -52,4 +91,4 @@ async function setAndGet(contractAddress) {
   console.log(`Stored value is: ${value}`);
 }
 
-setAndGet('your-contract-address');
+//setAndGet('0xb114516fB0E3aA01a44df82485f3729A119Ce9A9');
