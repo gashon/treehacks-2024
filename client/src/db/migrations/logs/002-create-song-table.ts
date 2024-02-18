e2bbd2039dd89b65a52e0195d80e8a9b694bef98
Ameya Jadhav <jadhavameyak@gmail.com>
2024-02-18 04:51:27 -0800

    updates

--- client/src/db/migrations/logs/002-create-song-table.ts
+++ client/src/db/migrations/logs/002-create-song-table.ts
@@ -1,31 +1,31 @@
-import { Kysely, sql } from "kysely";
+import { Kysely, sql } from 'kysely';
 
 export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
-    .createTable("song")
-    .addColumn("id", "uuid", (col) =>
-      col.primaryKey().defaultTo(sql`gen_random_uuid()`),
+    .createTable('song')
+    .addColumn('id', 'uuid', (col) =>
+      col.primaryKey().defaultTo(sql`gen_random_uuid()`)
     )
-    .addColumn("user_id", "uuid", (col) => col.notNull().references("user.id"))
-    .addColumn("file_type", "varchar", (col) => col.notNull())
-    .addColumn("file_name", "varchar", (col) => col.notNull())
-    .addColumn("s3_key", "varchar", (col) => col.notNull())
-    .addColumn("chain_address", "varchar", (col) => col.unique().notNull())
-    .addColumn("created_at", "timestamp", (col) =>
-      col.defaultTo(sql`now()`).notNull(),
+    .addColumn('user_id', 'uuid', (col) => col.notNull().references('user.id'))
+    .addColumn('file_type', 'varchar', (col) => col.notNull())
+    .addColumn('file_name', 'varchar', (col) => col.notNull())
+    .addColumn('s3_key', 'varchar', (col) => col.notNull())
+    .addColumn('chain_address', 'varchar', (col) => col.unique().notNull())
+    .addColumn('created_at', 'timestamp', (col) =>
+      col.defaultTo(sql`now()`).notNull()
     )
-    .addColumn("updated_at", "timestamp", (col) =>
-      col.defaultTo(sql`now()`).notNull(),
+    .addColumn('updated_at', 'timestamp', (col) =>
+      col.defaultTo(sql`now()`).notNull()
     )
     .execute();
 
   await db.schema
-    .createIndex("song_id_index")
-    .on("song")
-    .column("id")
+    .createIndex('song_id_index')
+    .on('song')
+    .column('id')
     .execute();
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
-  await db.schema.dropTable("user").execute();
+  await db.schema.dropTable('song').execute();
 }
--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -1,29 +1,28 @@
-import { useCallback, useState, useRef, FC, useEffect } from "react";
-import { useDropzone } from "react-dropzone";
-import { AiOutlineSound } from "react-icons/ai";
-import { Toaster, toast } from "sonner";
+import { useCallback, useState, useRef, FC, useEffect } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { AiOutlineSound } from 'react-icons/ai';
+import { Toaster, toast } from 'sonner';
 // import { WaveSurfer } from "wavesurfer-react";
 
-import { queryClient } from "@/lib/react-query";
+import { queryClient } from '@/lib/react-query';
 import {
   useUploadFile,
   getPresignedUrl,
   useGetSongs,
   uploadToChain,
   submitDMCAClaim,
-} from "@/features/song";
+} from '@/features/song';
 
 const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
 
   return (
     // Increased z-index for modal overlay to ensure it's above everything
-    <div className="bg-black bg-opacity-50 fixed flex inset-0 items-center justify-center z-50">
+    <div className='bg-black bg-opacity-50 fixed flex inset-0 items-center justify-center z-50'>
       <div
-        className="bg-white p-8 rounded-lg"
-        style={{ width: "60%", height: "auto", maxWidth: "800px", zIndex: 60 }}
-      >
-        {" "}
+        className='bg-white p-8 rounded-lg'
+        style={{ width: '60%', height: 'auto', maxWidth: '800px', zIndex: 60 }}>
+        {' '}
         {/* Adjusted size */}
         <button onClick={onClose}>X</button>
         {children}
@@ -37,14 +36,14 @@ const UploadingModal: React.FC<{ isOpen: boolean }> = ({ isOpen }) => {
   const [isAnimating, setIsAnimating] = useState<boolean>(false);
 
   const steps = [
-    "Opening media file",
-    "Verifying originality with Sonoverse ML",
-    "Generating presigned URL",
-    "Uploading file to storage",
-    "Uploading file to IPFS",
-    "Hashing file",
-    "Deploying chain contract",
-    "All complete",
+    'Opening media file',
+    'Verifying originality with Sonoverse ML',
+    'Generating presigned URL',
+    'Uploading file to storage',
+    'Uploading file to IPFS',
+    'Hashing file',
+    'Deploying chain contract',
+    'All complete',
   ];
 
   useEffect(() => {
@@ -69,28 +68,29 @@ const UploadingModal: React.FC<{ isOpen: boolean }> = ({ isOpen }) => {
   if (!isOpen && !isAnimating) return null;
 
   return (
-    <div className="bg-black bg-opacity-60 fixed flex inset-0 items-center justify-center text-black z-50">
-      <div className="bg-white max-w-2xl md:p-12 mx-4 p-8 rounded-lg shadow-xl w-full">
-        <div className="flex justify-center">
+    <div className='bg-black bg-opacity-60 fixed flex inset-0 items-center justify-center text-black z-50'>
+      <div className='bg-white max-w-2xl md:p-12 mx-4 p-8 rounded-lg shadow-xl w-full'>
+        <div className='flex justify-center'>
           {/* Inline style for the spinner animation */}
-          <div className="animate-spin border-b-2 border-blue-500 h-8 rounded-full w-8"></div>
+          <div className='animate-spin border-b-2 border-blue-500 h-8 rounded-full w-8'></div>
         </div>
-        <h2 className="font-semibold md:text-2xl mt-4 text-center text-xl">
+        <h2 className='font-semibold md:text-2xl mt-4 text-center text-xl'>
           Uploading...
         </h2>
-        <ul className="list-none mt-6 space-y-2">
+        <ul className='list-none mt-6 space-y-2'>
           {steps.slice(0, currentStepIndex).map((step, index) => (
-            <li key={index} className="flex items-center md:text-lg text-base">
+            <li
+              key={index}
+              className='flex items-center md:text-lg text-base'>
               <svg
-                className="h-6 mr-2 text-blue-500 w-6"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path d="M5 13l4 4L19 7"></path>
+                className='h-6 mr-2 text-blue-500 w-6'
+                fill='none'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                viewBox='0 0 24 24'
+                stroke='currentColor'>
+                <path d='M5 13l4 4L19 7'></path>
               </svg>
               {step}
             </li>
@@ -108,7 +108,7 @@ const Dropzone: FC = () => {
   const onDrop = useCallback(
     // TODO add notifications/loading indicator
     async (acceptedFiles) => {
-      setIsUploading(true); // TODO fix
+      setIsUploading(false); // TODO fix
 
       acceptedFiles.forEach(async (file: File) => {
         // Get presigned URL for each file
@@ -130,20 +130,20 @@ const Dropzone: FC = () => {
             s3Key: presignedResponse.key,
           });
 
-          queryClient.invalidateQueries({ queryKey: ["songs"] });
+          queryClient.invalidateQueries({ queryKey: ['songs'] });
 
           setIsUploading(false); // TODO fix
-          console.log("uploaded");
+          console.log('uploaded');
         }
       });
     },
-    [getPresignedUrl, uploadFileMutation],
+    [getPresignedUrl, uploadFileMutation]
   );
 
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
     onDrop,
     accept: {
-      "audio/*": [".mp3", ".wav", ".aac"],
+      'audio/*': ['.mp3', '.wav', '.aac'],
     },
   });
 
@@ -154,20 +154,19 @@ const Dropzone: FC = () => {
   ));
 
   return (
-    <section className="container mx-auto w-full">
+    <section className='container mx-auto w-full'>
       <UploadingModal isOpen={isUploading} />
       <div
         {...getRootProps({
           className:
-            "bg-zinc-900 hover:bg-zinc-800 w-full flex flex-col items-center justify-center p-10 border-2 border-dashed rounded-md cursor-pointer ",
-        })}
-      >
+            'bg-zinc-900 hover:bg-zinc-800 w-full flex flex-col items-center justify-center p-10 border-2 border-dashed rounded-md cursor-pointer ',
+        })}>
         <input {...getInputProps()} />
         <p>Click to upload your music</p>
       </div>
       {files?.length > 0 && (
-        <aside className="mt-4">
-          <h4 className="font-semibold text-lg">Files</h4>
+        <aside className='mt-4'>
+          <h4 className='font-semibold text-lg'>Files</h4>
           <ul>{files}</ul>
         </aside>
       )}
@@ -185,8 +184,22 @@ const AudioVisualizer: FC<{ url: string }> = ({ url }) => {
   return (
     <WaveSurfer
       url={url}
-      options={{ waveColor: "violet", progressColor: "purple" }}
+      options={{ waveColor: 'violet', progressColor: 'purple' }}
     />
+  );
+};
+
+const CopyrightLink: FC<{ url: string }> = ({ url }) => {
+  return (
+    <>
+      <a
+        className='break-all hover:underline text-blue-600'
+        target='_blank'
+        href={url}>
+        {url}
+      </a>
+      <SubmitClaimButton />
+    </>
   );
 };
 
@@ -214,7 +227,7 @@ const SongsList: FC = () => {
   const playAudio = async (song) => {
     try {
       const url = new URL(
-        `https://treehacks-2024.s3.us-west-1.amazonaws.com/${song.s3Key}`,
+        `https://treehacks-2024.s3.us-west-1.amazonaws.com/${song.s3Key}`
       );
       setAudioUrl(url);
 
@@ -223,7 +236,7 @@ const SongsList: FC = () => {
 
       setAudio(audio);
     } catch (error) {
-      console.error("Error playing the song", error);
+      console.error('Error playing the song', error);
     }
   };
 
@@ -232,68 +245,88 @@ const SongsList: FC = () => {
   };
 
   return (
-    <section className="flex flex-col gap-2">
-      <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+    <section className='flex flex-col gap-2'>
+      <Modal
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}>
         <>
-          <h2 className="font-bold text-xl">Copyright Infringements</h2>
-          <li key={`0`} className="flex flex-row items-center justify-between">
-            <a href="https://www.youtube.com/watch?v=R7-ApK63X_U&pp=ygUXaGFwcHkgY2xhcCBhbG9uZyBwYXJvZHk%3D">
-              https://www.youtube.com/watch?v=R7-ApK63X_U&pp=ygUXaGFwcHkgY2xhcCBhbG9uZyBwYXJvZHk%3D
-            </a>
-            <SubmitClaimButton />
+          <h2 className='font-bold mb-4 text-xl'>Copyright Infringements</h2>
+          <li
+            key={`0`}
+            className='flex flex-row items-center justify-between mb-2'>
+            <CopyrightLink
+              url={
+                'https://www.youtube.com/watch?v=R7-ApK63X_U&pp=ygUXaGFwcHkgY2xhcCBhbG9uZyBwYXJvZHk%3D'
+              }
+            />
           </li>
-          <li key={`1`} className="flex flex-row items-center justify-between">
-            <a href="https://www.youtube.com/watch?v=dM12YTZvQGw&pp=ygUXaGFwcHkgY2xhcCBhbG9uZyBwYXJvZHk%3D">
-              https://www.youtube.com/watch?v=dM12YTZvQGw&pp=ygUXaGFwcHkgY2xhcCBhbG9uZyBwYXJvZHk%3D
-            </a>
-            <SubmitClaimButton />
+          <li
+            key={`1`}
+            className='flex flex-row items-center justify-between mb-2'>
+            <CopyrightLink
+              url={
+                'https://www.youtube.com/watch?v=dM12YTZvQGw&pp=ygUXaGFwcHkgY2xhcCBhbG9uZyBwYXJvZHk%3D'
+              }
+            />
           </li>
-          <li key={`2`} className="flex flex-row items-center justify-between">
-            <a href="https://www.youtube.com/watch?v=Go_p6oD7AIE&pp=ygUWaGFwcHkgY2xhcCBhbG9uZyByZW1peA%3D%3D">
-              https://www.youtube.com/watch?v=Go_p6oD7AIE&pp=ygUWaGFwcHkgY2xhcCBhbG9uZyByZW1peA%3D%3D
-            </a>
-            <SubmitClaimButton />
+          <li
+            key={`2`}
+            className='flex flex-row items-center justify-between mb-2'>
+            <CopyrightLink
+              url={
+                'https://www.youtube.com/watch?v=Go_p6oD7AIE&pp=ygUWaGFwcHkgY2xhcCBhbG9uZyByZW1peA%3D%3D'
+              }
+            />
           </li>
-          <li key={`3`} className="flex flex-row items-center justify-between">
-            <a href="https://www.youtube.com/watch?v=Sj0DF0rRF18&pp=ygUWaGFwcHkgY2xhcCBhbG9uZyByZW1peA%3D%3D">
-              https://www.youtube.com/watch?v=Sj0DF0rRF18&pp=ygUWaGFwcHkgY2xhcCBhbG9uZyByZW1peA%3D%3D
-            </a>
-            <SubmitClaimButton />
+          <li
+            key={`3`}
+            className='flex flex-row items-center justify-between mb-2'>
+            <CopyrightLink
+              url={
+                'https://www.youtube.com/watch?v=Sj0DF0rRF18&pp=ygUWaGFwcHkgY2xhcCBhbG9uZyByZW1peA%3D%3D'
+              }
+            />
           </li>
-          <li key={`4`} className="flex flex-row items-center justify-between">
-            <a href="https://www.youtube.com/watch?v=BbWGlDi7M7M&pp=ygUWaGFwcHkgY2xhcCBhbG9uZyByZW1peA%3D%3D">
-              https://www.youtube.com/watch?v=BbWGlDi7M7M&pp=ygUWaGFwcHkgY2xhcCBhbG9uZyByZW1peA%3D%3D
-            </a>
-            <SubmitClaimButton />
+          <li
+            key={`4`}
+            className='flex flex-row items-center justify-between mb-2'>
+            <CopyrightLink
+              url={
+                'https://www.youtube.com/watch?v=BbWGlDi7M7M&pp=ygUWaGFwcHkgY2xhcCBhbG9uZyByZW1peA%3D%3D'
+              }
+            />
           </li>
-          <li key={`5`} className="flex flex-row items-center justify-between">
-            <a href="https://www.youtube.com/watch?v=SqJGCnfr7rc&pp=ygUWaGFwcHkgY2xhcCBhbG9uZyByZW1peA%3D%3D">
-              https://www.youtube.com/watch?v=SqJGCnfr7rc&pp=ygUWaGFwcHkgY2xhcCBhbG9uZyByZW1peA%3D%3D
-            </a>
-            <SubmitClaimButton />
+          <li
+            key={`5`}
+            className='flex flex-row items-center justify-between mb-2'>
+            <CopyrightLink
+              url={
+                'https://www.youtube.com/watch?v=SqJGCnfr7rc&pp=ygUWaGFwcHkgY2xhcCBhbG9uZyByZW1peA%3D%3D'
+              }
+            />
           </li>
         </>
       </Modal>
-      <p className="text-xl">Your Songs</p>
-      <ul className="flex flex-col gap-10">
+      <p className='text-xl'>Your Songs</p>
+      <ul className='flex flex-col gap-10'>
         {data.data.songs.map((song, index) => {
           const isFirst = index === data.data.songs.length - 1;
           return (
             <li
               key={`song:${song.id}`}
-              className="flex flex-row items-center justify-between"
-            >
+              className='flex flex-row items-center justify-between'>
               {isFirst ? (
                 <a
-                  className="cursor-pointer hover:underline text-blue-600 text-lg"
-                  onClick={() => handleNotificationsOpen()}
-                >
+                  className='cursor-pointer hover:underline text-blue-600 text-lg'
+                  onClick={() => handleNotificationsOpen()}>
                   {song.fileName}
                 </a>
               ) : (
-                <p className="text-lg">{song.fileName}</p>
+                <p className='text-lg'>{song.fileName}</p>
               )}
-              <div className="cursor-pointer" onClick={() => playAudio(song)}>
+              <div
+                className='cursor-pointer'
+                onClick={() => playAudio(song)}>
                 <AiOutlineSound />
               </div>
             </li>
@@ -305,8 +338,8 @@ const SongsList: FC = () => {
 };
 
 const Banner: FC = () => (
-  <header className="mb-10">
-    <h1 className="text-3xl">Sonoverse</h1>
+  <header className='mb-10'>
+    <h1 className='text-3xl'>Sonoverse</h1>
   </header>
 );
 
@@ -315,14 +348,14 @@ const SubmitClaimButton: FC = () => {
 
   const handleClick = () => {
     setIsDisabled(true);
-    const dmcaSubmissionToast = toast("Submitting DMCA claim...");
+    const dmcaSubmissionToast = toast('Submitting DMCA claim...');
 
-    toast.loading("Submitting DMCA claim... ", {
+    toast.loading('Submitting DMCA claim... ', {
       id: dmcaSubmissionToast,
     });
 
     setTimeout(() => {
-      toast.success("Submitted DMCA claim", {
+      toast.success('Submitted DMCA claim', {
         id: dmcaSubmissionToast,
       });
     }, 1500);
@@ -330,13 +363,12 @@ const SubmitClaimButton: FC = () => {
 
   return (
     <button
-      id="submitClaimBtn"
+      id='submitClaimBtn'
       disabled={isDisabled}
       onClick={handleClick}
       className={`bg-yellow-400 border-1 border-black font-bold px-3 py-1 rounded-full text-black ${
-        isDisabled ? "opacity-50 cursor-not-allowed" : "hover:bg-yellow-500"
-      }`}
-    >
+        isDisabled ? 'opacity-50 cursor-not-allowed' : 'hover:bg-yellow-500'
+      }`}>
       Submit Claim
     </button>
   );
@@ -344,13 +376,13 @@ const SubmitClaimButton: FC = () => {
 
 export default function Home() {
   return (
-    <main className="800 flex justify-center min-h-screen mt-10 w-full">
+    <main className='800 flex justify-center min-h-screen mt-10 w-full'>
       <Toaster />
 
-      <div className="lg:w-3/4 w-11/12">
+      <div className='lg:w-3/4 w-11/12'>
         <Banner />
         <Dropzone />
-        <div className="mt-10">
+        <div className='mt-10'>
           <SongsList />
         </div>
       </div>
